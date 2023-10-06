96488c459199983b44764e152dcb534549f51f77
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-10-06 12:37:51 +0200

    props for entits

--- src/SPG.Vogi.Recommendation/Entity.cs
+++ src/SPG.Vogi.Recommendation/Entity.cs
@@ -1,4 +1,4 @@
-﻿namespace SPG.Vogi.Recommendation
+﻿namespace SPG.Vogi.Recommendation.DomainModel
 {
     public class User
     {
@@ -6,9 +6,23 @@
         public Guid Guid { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        
+        public List<Posts> Posts{ get; set; } = new List<Posts>();
+        public List<Posts> LikesPosts { get; set; } = new List<Posts>();
+
+
 
     }
-    public class Posts { }
+
+    public class Posts
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public User user { get; set; }
+        public String Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<User> Likes { get; set; }
+        public Dictionary<User, String> Comments { get; set; } = new Dictionary<User, String>();
+
+    }
 
 }
