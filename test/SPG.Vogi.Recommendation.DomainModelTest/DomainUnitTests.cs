4259c95189c8e5d0d81d403b2035741bd1d5841c
Marco-Robert <94784771+devmarcorobert@users.noreply.github.com>
2023-10-09 11:24:58 +0200

    feat: add xunit DomainTest class

---
+++ test/SPG.Vogi.Recommendation.DomainModelTest/DomainUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPG.Vogi.Recommendation.DomainModelTest
+{
+    public class DomainUnitTests
+    {
+        [Fact]
+        public void TestUser()
+        {
+           
+        }
+    }
+}
