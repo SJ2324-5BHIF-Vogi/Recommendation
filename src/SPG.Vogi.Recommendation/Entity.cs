f6c91882bac571d0f00789cbd7a3e1e9bc0ae51f
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-11-24 12:04:29 +0100

    umsortiert

--- src/SPG.Vogi.Recommendation/Entity.cs
+++ src/SPG.Vogi.Recommendation/Entity.cs
@@ -21,20 +21,5 @@
 
     }
 
-    public class Posts
-    {
-        public int Id { get; set; }
-        public Guid Guid { get; set; }
-        public User user { get; set; }
-        public String Content { get; set; }
-        public DateTime CreatedAt { get; set; }
-        private List<User> _likes { get; set; } = new();
-        
-        //Lists
-        public IReadOnlyList<User> Likes => _likes;
-
-        //public Dictionary<User, String> Comments { get; set; } = new Dictionary<User, String>();
-
-    }
-
+    
 }
