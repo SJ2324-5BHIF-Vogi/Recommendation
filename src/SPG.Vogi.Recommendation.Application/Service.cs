d49cd7e72c7e2fbe17ce6ab307f3af1c70caafdb
David Ankenbrand <david.ankenbrand98@gmail.com>
2024-01-08 11:31:27 +0100

    add random Get Posts

--- src/SPG.Vogi.Recommendation.Application/Service.cs
+++ src/SPG.Vogi.Recommendation.Application/Service.cs
@@ -1,12 +1,19 @@
 ﻿using SPG.Vogi.Recommendation.DomainModel;
 using SPG.Vogi.Recommendation.Repository;
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace SPG.Vogi.Recommendation.Application
 {
     public class RecommService
     {
+        MongoRepository<Posts> _mongoRepository;
+        public RecommService(MongoRepository<Posts> mongoRepository)
+        {
+            _mongoRepository = mongoRepository;
+        }
+
         public void Test() {
             MongoDbSettings mongoDbSettings = new MongoDbSettings();
             //user:pw @host
@@ -14,7 +21,8 @@ namespace SPG.Vogi.Recommendation.Application
             mongoDbSettings.DatabaseName = "mongodb";
 
             //MongoRepository<Person> _peopleRepository = new MongoRepository<Person>(mongoDbSettings);
-            MongoRepository<Posts> mongoRepository = new MongoRepository<Posts> (mongoDbSettings);  
+            MongoRepository<Posts> mongoRepository = new MongoRepository<Posts> (mongoDbSettings); 
+            
         }
 
         public List<string> serachForHashTags(string input) 
@@ -28,6 +36,20 @@ namespace SPG.Vogi.Recommendation.Application
                 hashTags.Add(match.ToString());
             }
             return hashTags;
+        }
+        public List<Posts> getPosts(int Userid)
+        {
+            var posts =  _mongoRepository.AsQueryable().ToList();
+            var random = new Random();
+            int index = random.Next(posts.Count);
+
+            List<Posts> returnvalue = new List<Posts>();
+            for(int i = 0; i <= 10; i++)
+            {
+                index = random.Next(posts.Count);
+                returnvalue.Add(posts[index]);
+            }
+            return returnvalue;
         }
 
     }
--- src/SPG.Vogi.Recommendation.TextAnalyseTest/UnitTest1.cs
+++ src/SPG.Vogi.Recommendation.TextAnalyseTest/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
 using Lucene.Net.Index;
+using Lucene.Net.Search;
 using Lucene.Net.Store;
 using Lucene.Net.Util;
 
@@ -26,6 +28,56 @@ namespace SPG.Vogi.Recommendation.TextAnalyseTest
             // Create an index writer
             var indexConfig = new IndexWriterConfig(AppLuceneVersion, analyzer);
             using var writer = new IndexWriter(dir, indexConfig);
+        }
+        public void Query() 
+        {
+            // Search with a phrase
+            var phrase = new MultiPhraseQuery
+                {
+                    new Term("favoritePhrase", "brown"),
+                    new Term("favoritePhrase", "fox")
+                };
+        }
+        public void addToIndex(IndexWriter writer) 
+        {
+            var source = new
+            {
+                Name = "Kermit the Frog",
+                FavoritePhrase = "The quick brown fox jumps over the lazy dog"
+            };
+            var doc = new Document
+            {
+                // StringField indexes but doesn't tokenize
+                new StringField("name",
+                    source.Name,
+                    Field.Store.YES),
+                new TextField("favoritePhrase",
+                    source.FavoritePhrase,
+                    Field.Store.YES)
+            };
+
+            writer.AddDocument(doc);
+            writer.Flush(triggerMerge: false, applyAllDeletes: false);
+        }
+
+        public void FetchResult(IndexWriter writer)
+        {
+            // Re-use the writer to get real-time updates
+            using var reader = writer.GetReader(applyAllDeletes: true);
+            var searcher = new IndexSearcher(reader);
+            var hits = searcher.Search(phrase, 20 /* top 20 */).ScoreDocs;
+
+            // Display the output in a table
+            Console.WriteLine($"{"Score",10}" +
+                $" {"Name",-15}" +
+                $" {"Favorite Phrase",-40}");
+            foreach (var hit in hits)
+            {
+                var foundDoc = searcher.Doc(hit.Doc);
+                Console.WriteLine($"{hit.Score:f8}" +
+                    $" {foundDoc.Get("name"),-15}" +
+                    $" {foundDoc.Get("favoritePhrase"),-40}");
+            }
         }
     }
 }
