b12c95fdc353b32c90840501ac083eca8ea5fcac
Marco-Robert <94784771+devmarcorobert@users.noreply.github.com>
2024-01-22 10:58:57 +0100

    feat: add changes

--- test/SPG.Vogi.Recommendation.UnitTests/DomainModelTests.cs
+++ test/SPG.Vogi.Recommendation.UnitTests/DomainModelTests.cs
@@ -37,5 +37,30 @@ public class DomainModelTests
         Assert.Equal(1,user01.Followers.Count);
     }
     
-    
+    [Fact]
+    public void TestAddPost()
+    {
+        var user01 = new User("username1", "email1");
+        var user02 = new User("username2", "email2");
+
+        var post01 = new Posts(user01, "Hello", new DateTime(2000, 01, 01));
+
+        post01.AddLike(user02);
+        
+        Assert.Equal(1, post01.Likes.Count);
+    }
+
+    [Fact]
+    public void TestRemovePost()
+    {
+        var user01 = new User("username1", "email1");
+        var user02 = new User("username2", "email2");
+
+        var post01 = new Posts(user01, "Hello", new DateTime(2000, 01, 01));
+
+        post01.AddLike(user02);
+        post01.RemoveLike(user02);
+        
+        Assert.Equal(0, post01.Likes.Count);
+    }
 }
